7ebd4f737e78d67b21dcc10aa4c798276c3b205a
Simone Ballard <simone.a.ballard@gmail.com>
2021-02-10 22:33:13 -0700

    deployment test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <Router>
@@ -11,9 +10,4 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
 
