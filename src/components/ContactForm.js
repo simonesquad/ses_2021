2f3fdbc4154d9b77b9874db8763ac3bbd6b61094
Simone Ballard <simone.a.ballard@gmail.com>
2021-02-14 15:57:45 -0700

    css updated for contact form, still need to resolve one font issue

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -22,21 +22,23 @@ export default function ContactForm({backgroundImg}) {
                 backgroundImage: `url(${backgroundImg})`
             }}>
             <form onSubmit={sendEmail}>
+            <div className="contents">
                 <div className="row pt-5 mx-auto">
                     <div className="col-8 form-group mx-auto">
                         <input type="text" className="form-control" placeholder="Name" name="name" />
                     </div>
                     <div className="col-8 form-group mx-auto">
-                        <input type="email" className="form-control" placeholder="Email Address" name="email" />
+                        <input type="email" className="form-control" placeholder="Email" name="email" />
                     </div>
                     <div className="col-8 form-group mx-auto">
                         <input type="text" className="form-control" placeholder="Subject" name="subject" />
                     </div>
                     <div className="col-8 form-group mx-auto">
-                        <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
+                        <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message here" name="message"></textarea>
                     </div>
                     <div className="col-8 pt-3 mx-auto">
-                        <input type="submit" className="btn btn-info" value="Send Message"></input>
+                        <input type="submit" className="btn btn-info" value="Contact Us"></input>
+                        </div>
                     </div>
                 </div>
             </form>
