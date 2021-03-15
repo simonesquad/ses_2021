2ac051fd2fdc5fd4a5807036a93b1a61e9547afb
Simone Ballard <simone.a.ballard@gmail.com>
2021-03-14 21:29:54 -0600

    banner on home page items

--- src/components/Item.js
+++ src/components/Item.js
@@ -9,10 +9,8 @@ const Item = ({title, desc, backgroundImg}) => {
         <div className="item__container">
         <div className="item__text">
                 <p>{title}</p>
-        <div className="item__textDesc">
                 <p>{desc}</p>
                 </div>
-            </div>
         </div>
     </div>
     )
